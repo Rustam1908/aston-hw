6ec7fb81f8233c2ea2957ab42aef0c35c88508b2
Rustam <shekhmametev.r@gmail.com>
2023-10-25 12:41:21 +0300

    ref: lombok

--- src/main/java/org/rusty/entity/Course.java
+++ src/main/java/org/rusty/entity/Course.java
@@ -1,43 +1,15 @@
 package org.rusty.entity;
 
+import lombok.Data;
+
 import java.util.List;
 
+@Data
 public class Course {
 
     private int courseId;
     private String title;
     private int duration;
     private List<Student> students;
-
-    public int getCourseId() {
-        return courseId;
-    }
-
-    public void setCourseId(int courseId) {
-        this.courseId = courseId;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public int getDuration() {
-        return duration;
-    }
-
-    public void setDuration(int duration) {
-        this.duration = duration;
-    }
-
-    public List<Student> getStudents() {
-        return students;
-    }
-
-    public void setStudents(List<Student> students) {
-        this.students = students;
-    }
+    private List<Teacher> teachers;
 }
--- src/main/java/org/rusty/entity/Student.java
+++ src/main/java/org/rusty/entity/Student.java
@@ -1,43 +1,14 @@
 package org.rusty.entity;
 
+import lombok.Data;
+
 import java.util.List;
 
+@Data
 public class Student {
 
     private int studentId;
     private String firstName;
     private String lastName;
     private List<Course> courses;
-
-    public int getStudentId() {
-        return studentId;
-    }
-
-    public void setStudentId(int studentId) {
-        this.studentId = studentId;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public List<Course> getCourses() {
-        return courses;
-    }
-
-    public void setCourses(List<Course> courses) {
-        this.courses = courses;
-    }
 }
