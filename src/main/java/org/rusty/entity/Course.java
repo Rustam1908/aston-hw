6ead18824ae255f7bab07b67a694ae2c27b59a29
Rustam <shekhmametev.r@gmail.com>
2023-10-25 11:12:55 +0300

    feat: new entities

---
+++ src/main/java/org/rusty/entity/Course.java
@@ -0,0 +1,34 @@
+package org.rusty.entity;
+
+import java.util.List;
+
+public class Course {
+
+    private String name;
+    private int duration;
+    private List<Student> students;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(List<Student> students) {
+        this.students = students;
+    }
+}
---
+++ src/main/java/org/rusty/entity/Student.java
@@ -0,0 +1,34 @@
+package org.rusty.entity;
+
+import java.util.List;
+
+public class Student {
+
+    private String firstName;
+    private String lastName;
+    private List<Course> courses;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public List<Course> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(List<Course> courses) {
+        this.courses = courses;
+    }
+}
