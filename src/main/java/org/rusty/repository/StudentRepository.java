194d1136643329fbad5a8123317265a966df4818
Rustam <shekhmametev.r@gmail.com>
2023-10-30 20:44:07 +0300

    feat: StudentRepository hibernate find all impl

--- src/main/java/org/rusty/repository/StudentRepository.java
+++ src/main/java/org/rusty/repository/StudentRepository.java
@@ -1,14 +1,31 @@
 package org.rusty.repository;
 
+import lombok.extern.slf4j.Slf4j;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.rusty.entity.Student;
+import org.rusty.service.SessionFactoryProvider;
 
-import java.util.Collections;
 import java.util.List;
 
+@Slf4j
 public class StudentRepository {
 
-    public List<Student> findAll() {
+    private final SessionFactory factory = SessionFactoryProvider.getSessionFactory();
 
-        return Collections.emptyList();
+    public List<Student> findAll() {
+        Transaction transaction = null;
+        List<Student> students = null;
+        try (Session session = factory.openSession()) {
+            transaction = session.beginTransaction();
+            students = session.createQuery("SELECT s FROM Student s", Student.class).getResultList();
+            transaction.commit();
+        } catch (HibernateException e) {
+            if (transaction != null) transaction.rollback();
+            log.error(e.getMessage());
+        }
+        return students;
     }
 }
